b36252143bf54037ce1154840296c3766b0335d3
Christophe <dufour.chri@gmail.com>
2022-09-22 18:01:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Change template
+
+in index.html
+
+```HTML
+<html class="theme-1 dark" />
+```
+
+Choices :
+
+- default
+- theme-1
+- theme-2
+- theme-3
+- theme-4
+- theme-5
---
+++ src/layouts/side-menu/app.js
@@ -0,0 +1,14 @@
+const menu = [
+  {
+    icon: 'Home',
+    pathname: '/',
+    title: 'Page 1'
+  },
+  {
+    icon: 'Home',
+    pathname: '/page-2',
+    title: 'Page 2'
+  }
+]
+
+export { menu }
